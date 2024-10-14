6bd4e58ad0367c2680591ca54fffe3687df3d823
LucasSanchez82 <08.lucas.sanchez@gmail.com>
2024-10-14 21:06:03 +0200

    refacto: update Profile component to display user initials instead of avatar

--- components/profile/Profile.tsx
+++ components/profile/Profile.tsx
@@ -7,18 +7,16 @@ const Profile = async () => {
   const session = await getSession();
   const image = session.user?.image;
   const name = session.user?.name;
-  const avatarFallack = name
+  const initials = name
     ?.split(" ")
     .map((n) => n[0])
     .slice(0, 2)
-    .join("");
+    .join("")
+    ?.toUpperCase();
   return (
     <ProfileSettingsPopover>
       <section className="w-12 h-12 flex justify-center items-center bg-gray-300 rounded-full overflow-hidden absolute top-2 right-5 cursor-pointer text-black">
-        <Avatar>
-          <AvatarImage src={"image!"} alt={name || "nom utilisateur"} />
-          <AvatarFallback>{avatarFallack || "null"}</AvatarFallback>
-        </Avatar>
+      {initials}
       </section>
     </ProfileSettingsPopover>
   );
