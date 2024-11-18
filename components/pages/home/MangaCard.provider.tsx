e5e24281de06654cfd63e9985cff58769c705b4f
LucasSanchez82 <jiraya1008@gmail.com>
2024-11-18 21:42:36 +0100

    feat: refactor MangaCard component to use MangaCardProvider for improved structure and readability

---
+++ components/pages/home/MangaCard.provider.tsx
@@ -0,0 +1,39 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { BookOpen } from "lucide-react";
+import Link from "next/link";
+import MangaCardBackgroundImage from "./MangaCard.backgroundImage";
+import { PersonnalManga } from "@/lib/cachedRequests/manga/getPersonnalMangas";
+import { PropsWithChildren } from "react";
+
+export default function MangaCardProvider({
+  title,
+  readerUrl,
+  image,
+  chapter,
+  isSelfHosted,
+  description,
+  id,
+  children,
+}: PropsWithChildren<PersonnalManga>) {
+  const imageUrl = `${isSelfHosted ? process.env.SELFHOSTED_IMAGES_BASE_URL + "/" : ""}${image}`;
+  return (
+    <Card className="w-full max-w-sm h-64 overflow-hidden group relative">
+      <MangaCardBackgroundImage imageUrl={imageUrl} />
+      <div className="absolute inset-0 bg-black bg-opacity-50 transition-opacity duration-300 group-hover:bg-opacity-70" />
+      <CardContent className="relative h-full flex flex-col justify-end p-4 text-white">
+        <h2 className="text-2xl font-bold mb-2 s">{title}</h2>
+        <p className="text-sm mb-4">Last read: {chapter}</p>
+        <div className="flex justify-between items-center">
+          <Link href={readerUrl} passHref>
+            <Button className="flex-grow mr-2" variant="secondary">
+              <BookOpen className="mr-2 h-4 w-4" />
+              Continue Reading
+            </Button>
+          </Link>
+          {children}
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
--- components/pages/home/MangaCard.tsx
+++ components/pages/home/MangaCard.tsx
@@ -1,59 +1,25 @@
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { BookOpen, Edit2 } from "lucide-react";
-import Link from "next/link";
+import { Edit2 } from "lucide-react";
 import MangaCardEditedForm from "../../forms/mangaForm/MangaCardEditedForm";
 import { DialogResponsive } from "@/components/global/DialogResponsive/DialogResponsive";
-import { Manga } from "@prisma/client";
-import MangaCardBackgroundImage from "./MangaCard.backgroundImage";
 import { PersonnalManga } from "@/lib/cachedRequests/manga/getPersonnalMangas";
+import MangaCardProvider from "./MangaCard.provider";
 
-export default function MangaCard({
-  title,
-  readerUrl,
-  image,
-  chapter,
-  isSelfHosted,
-  description,
-  id,
-}: PersonnalManga) {
-  const imageUrl = `${isSelfHosted ? process.env.SELFHOSTED_IMAGES_BASE_URL + "/" : ""}${image}`;
+export default function MangaCard(props: PersonnalManga) {
+  const imageUrl = `${props.isSelfHosted ? process.env.SELFHOSTED_IMAGES_BASE_URL + "/" : ""}${props.image}`;
   return (
-    <Card className="w-full max-w-sm h-64 overflow-hidden group relative">
-      <MangaCardBackgroundImage imageUrl={imageUrl} />
-      <div className="absolute inset-0 bg-black bg-opacity-50 transition-opacity duration-300 group-hover:bg-opacity-70" />
-      <CardContent className="relative h-full flex flex-col justify-end p-4 text-white">
-        <h2 className="text-2xl font-bold mb-2 s">{title}</h2>
-        <p className="text-sm mb-4">Last read: {chapter}</p>
-        <div className="flex justify-between items-center">
-          <Link href={readerUrl} passHref>
-            <Button className="flex-grow mr-2" variant="secondary">
-              <BookOpen className="mr-2 h-4 w-4" />
-              Continue Reading
-            </Button>
-          </Link>
-          <DialogResponsive
-            title={`Modifier ${title}`}
-            form={
-              <MangaCardEditedForm
-                {...{
-                  title,
-                  readerUrl,
-                  image: image || undefined,
-                  imageUrl,
-                  chapter,
-                  description,
-                  id,
-                }}
-              />
-            }
-            buttonProps={{ variant: "outline", size: "icon" }}
-            className="bg-cover bg-center"
-          >
-            <Edit2 className="h-4 w-4 dark:text-white text-black" />
-          </DialogResponsive>
-        </div>
-      </CardContent>
-    </Card>
+    <MangaCardProvider {...props}>
+      <DialogResponsive
+        title={`Modifier ${props.title}`}
+        form={
+          <MangaCardEditedForm
+            {...{ ...props, imageUrl, image: props.image || undefined }}
+          />
+        }
+        buttonProps={{ variant: "outline", size: "icon" }}
+        className="bg-cover bg-center"
+      >
+        <Edit2 className="h-4 w-4 dark:text-white text-black" />
+      </DialogResponsive>
+    </MangaCardProvider>
   );
 }
