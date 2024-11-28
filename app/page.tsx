eed011defc55b83dfb3e6e5bdeaddd856e837d86
LucasSanchez82 <08.lucas.sanchez@gmail.com>
2024-11-28 18:31:53 +0100

    chore: update package dependencies to latest versions

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import CallToAction from "@/components/CallToAction";
 import Features from "@/components/Features";
-import Hero from "@/components/Hero";
+import Hero from "@/components/hero";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
