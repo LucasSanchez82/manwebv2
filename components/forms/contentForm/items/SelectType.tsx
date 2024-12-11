12b33ffa3c89cb61a5d20353b03ca7364074a036
LucasSanchez82 <08.lucas.sanchez@gmail.com>
2024-12-11 12:01:24 +0100

    feat: add content type selection to AddContentForm and update validation schema

---
+++ components/forms/contentForm/items/SelectType.tsx
@@ -0,0 +1,30 @@
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectGroup,
+  SelectLabel,
+  SelectItem,
+} from "@/components/ui/select";
+import React from "react";
+
+export const SelectType = (
+  props: Omit<React.ComponentProps<typeof Select>, "children">
+) => (
+  <Select {...props}>
+    <SelectTrigger className="w-[180px]">
+      <SelectValue placeholder="Select a fruit" />
+    </SelectTrigger>
+    <SelectContent>
+      <SelectGroup>
+        <SelectLabel>Fruits</SelectLabel>
+        <SelectItem value="apple">Apple</SelectItem>
+        <SelectItem value="banana">Banana</SelectItem>
+        <SelectItem value="blueberry">Blueberry</SelectItem>
+        <SelectItem value="grapes">Grapes</SelectItem>
+        <SelectItem value="pineapple">Pineapple</SelectItem>
+      </SelectGroup>
+    </SelectContent>
+  </Select>
+);
