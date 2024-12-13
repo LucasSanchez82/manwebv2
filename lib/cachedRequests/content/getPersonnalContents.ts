ae096c0b32cc21122c225e52a9a4305cf001f8ae
LucasSanchez82 <jiraya1008@gmail.com>
2024-12-13 13:13:29 +0100

    refactor: simplify PersonnalContent type by removing unnecessary NullablePartial definition

--- lib/cachedRequests/content/getPersonnalContents.ts
+++ lib/cachedRequests/content/getPersonnalContents.ts
@@ -57,13 +57,4 @@ export type PersonnalContents = Awaited<
 >["contents"];
 
 type PersonnalContentPrimitive = PersonnalContents[number];
-
-type NullablePartial<
-  T,
-  NK extends keyof T = {
-    [K in keyof T]: null extends T[K] ? K : never;
-  }[keyof T],
-  NP = Partial<Pick<T, NK>> & Pick<T, Exclude<keyof T, NK>>,
-> = { [K in keyof NP]: NP[K] };
-
-export type PersonnalContent = NullablePartial<PersonnalContentPrimitive>;
+export type PersonnalContent = PersonnalContentPrimitive;
