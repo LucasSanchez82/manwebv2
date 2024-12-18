dfc552427392ccdab74f3b08f92005ad6197af6d
LucasSanchez82 <jiraya1008@gmail.com>
2024-12-18 10:07:05 +0000

    fix: request with /?

--- lib/actions/external/mangadex.action.ts
+++ lib/actions/external/mangadex.action.ts
@@ -4,7 +4,7 @@ export const getMangasFromMangadexAction = async (
   title: string,
   limit: number = 5
 ) => {
-  const endpoint = `https://api.mangadex.org/manga/`
+  const endpoint = `https://api.mangadex.org/manga`
   const requestedEndPoint = `${endpoint}?title=${title}&includes[]=cover_art&limit=${limit}`
   const response = await fetch(requestedEndPoint)
   return mangaResponseSchema.parse(await response.json())
