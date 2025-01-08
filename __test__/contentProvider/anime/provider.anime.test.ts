ec9ec9d076ed0b3aa163d748ef6176b36c9d0b59
LucasSanchez82 <jiraya1008@gmail.com>
2025-01-08 20:13:43 +0100

    en cours

---
+++ __test__/contentProvider/anime/provider.anime.test.ts
@@ -0,0 +1,7 @@
+import { describe, it } from 'bun:test'
+
+describe('provider.film', () => {
+  it('should return all content type ids available', () => {
+    fetch('https://anime-sama.fr/template-php/defaut/fetch.php', {})
+  })
+})
