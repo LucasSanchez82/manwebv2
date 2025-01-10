aa34b75de3140952c2a564264c618068ea24297f
LucasSanchez82 <jiraya1008@gmail.com>
2025-01-10 13:25:37 +0100

    feat: add loading state and error handling to EasyUpChapterButton for improved user experience

--- components/forms/contentForm/EasyUpChapterButton.tsx
+++ components/forms/contentForm/EasyUpChapterButton.tsx
@@ -1,7 +1,10 @@
 'use client'
+import Spinner from '@/components/global/Spinner'
 import { Button } from '@/components/ui/button'
 import { quickChangeChapterContent } from '@/lib/actions/contents.actions'
 import { ChevronUp, ChevronDown } from 'lucide-react'
+import { useState } from 'react'
+import { toast } from 'sonner'
 
 type Props = {
   idContent: number | bigint
@@ -9,12 +12,30 @@ type Props = {
 }
 
 const EasyUpChapterButton = ({ idContent, chapter }: Props) => {
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleFileChange = async (toUp: 1 | -1) => {
+    try {
+      setIsLoading(true)
+      await quickChangeChapterContent(idContent, toUp > 0)
+    } catch (error) {
+      toast.error('Erreur lors de la mise a jour du chapitre')
+    } finally {
+      setIsLoading(false)
+    }
+  }
+  if (isLoading)
+    return (
+      <div className="absolute left-1/2 top-2 -translate-x-1/2">
+        <Spinner />
+      </div>
+    )
   return (
-    <div className="flex w-min items-center gap-2 rounded p-1 backdrop-blur-sm">
+    <div className="absolute left-1/2 top-2 flex w-min -translate-x-1/2 items-center gap-2 rounded p-1 backdrop-blur-sm">
       <Button
         variant="ghost"
         size="icon"
-        onClick={async () => await quickChangeChapterContent(idContent, true)}
+        onClick={async () => await handleFileChange(1)}
         className="h-8 w-8 bg-white/10 text-white hover:bg-white/20"
       >
         <ChevronUp className="h-4 w-4" />
@@ -25,7 +46,7 @@ const EasyUpChapterButton = ({ idContent, chapter }: Props) => {
       <Button
         variant="ghost"
         size="icon"
-        onClick={async () => await quickChangeChapterContent(idContent, false)}
+        onClick={async () => await handleFileChange(-1)}
         className="h-8 w-8 bg-white/10 text-white hover:bg-white/20"
       >
         <ChevronDown className="h-4 w-4" />
